5be1820370fabdcba1c3af4153b9d717fc97ff81
ianarsenault <ian_arsenault@protonmail.com>
2018-03-18 00:13:01 -0400

    testing server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const logger = require('morgan')
+
+const app = express()
+app.use(logger('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/test', (req, res) => {
+    res.send([{
+        title: 'Hello, World!',
+        description: 'This is a server test! Bleep Bloop!'
+    }])
+})
+
+// app.listen(process.env.PORT || 8081)
+
+module.exports = app;
