11b0858e9d74abc3d646191e133bf3273b460c5a
Jay DeLuca <jaydeluca4@gmail.com>
2018-03-20 22:35:07 -0400

    Made navigation into its own component, added svg logo, more styling stuff

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="navbar is-transparent">
     <div class="navbar-brand">
-      <a class="navbar-item">
-        <!-- TIER 3 RESOLUTIONS FROM ABOVE LOGO -->
-      </a>
+      <router-link v-bind:to="{ name: 'Landing' }" class="navbar-item" exact>
+        <img :src="logo" alt="Tier 3" style="width: 100px; height: auto;">
+      </router-link>
       <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
         <span></span>
         <span></span>
@@ -13,29 +13,48 @@
 
     <div id="navbarExampleTransparentExample" class="navbar-menu">
       <div class="navbar-start">
-        <i class="far fa-hand-rock fa-7x"></i>
-        <i class="far fa-hand-paper fa-7x"></i>
-        <i class="far fa-hand-scissors fa-7x"></i>
+        <router-link v-bind:to="{ name: 'LeaderBoard' }" class="navbar-item" active-class="link-is-active">
+           <span class="icon">
+              <i class="fas fa-chart-line"></i>
+           </span>
+          <span>Leaderboard</span>
+        </router-link>
+        <router-link v-bind:to="{ name: 'Players' }" class="navbar-item" active-class="link-is-active">
+          <span class="icon">
+            <i class="fas fa-users"></i>
+          </span>
+          <span>Players</span>
+        </router-link>
       </div>
 
       <div class="navbar-end">
         <div class="navbar-item">
-          <div class="field">
-            <p class="control">
-              <a class="button is-primary" target="_blank" href="https://github.com/ianarsenault/Pulse-Roshambo">
-              <span class="icon">
-                <i class="fas fa-download"></i>
-              </span>
-                <span>Github</span>
-              </a>
-            </p>
-          </div>
+          <router-link v-bind:to="{ name: 'NewPlayer' }" class="button">
+               <span class="icon">
+                    <i class="fas fa-plus"></i>
+               </span>
+            <span>New Player</span>
+          </router-link>
         </div>
       </div>
-
     </div>
   </nav>
 </template>
+
+<script>
+
+  import logo from '../assets/images/death2.svg';
+
+  export default {
+    name: 'Navigation',
+    data() {
+      return {
+        logo: logo
+      }
+    }
+  }
+
+</script>
 
 <style lang="css">
   .far {
