d2aed85fc0a44c870cb987801fe1bcaf1e5a3922
Jonathan Keys <jonkeys11@gmail.com>
2018-03-28 20:26:50 -0400

    Updates to leaderboard endpoints to udpate and create leaderboard records

--- server/models/leaderboard.js
+++ server/models/leaderboard.js
@@ -3,12 +3,21 @@ const factory = require('./database.js');
 const schema = {
   player: String,
   wins: Number,
-  loses: Number,
+  losses: Number,
   conquerer: String,
   nemesis: String
 };
 
 const Leaderboard = factory("Leaderboard", schema);
+
+function createPlayerLeaderBoard() {
+    return new Promise((resolve, reject) => {
+        Leaderboard.find({}, Object.keys(schema).join(" "), function (error, leaderboard) {
+           if (error) { reject(error); }
+            resolve(leaderboard);
+        }).sort({_id:-1});
+    });
+}
 
 function fetchLeaderboard() {
     return new Promise((resolve, reject) => {
@@ -26,9 +35,28 @@ function fetchPlayerLeaderboard(id) {
             resolve(leaderboard);
         })
     });
+  }
+
+  function updatePlayerLeaderboard(id) {
+    return new Promise((resolve, reject) => {
+        Leaderboard.fetchPlayerLeaderboard(id, Object.keys(schema).join(" "), function (error, player) {
+            if (error) {reject(error) }
+
+            player.name = playerObj.name;
+            player.nickname = playerObj.nickname;
+            player.chant = playerObj.chant;
+
+            player.save(function (error) {
+                if (error) { reject(error); }
+                resolve(true);
+            });
+        });
+    });
 }
 
 module.exports = {
   fetchLeaderboard,
-  fetchPlayerLeaderboard
+  fetchPlayerLeaderboard,
+  updatePlayerLeaderboard,
+  createPlayerLeaderBoard
 };
--- server/routes/leaderboard.js
+++ server/routes/leaderboard.js
@@ -1,6 +1,19 @@
 let Leaderboard = require("../models/leaderboard");
 
 module.exports = (app) => {
+
+  // Add new player
+  app.post('/leaderboard/player/:id', (req, res) => {
+    Leaderboard.createPlayerLeaderboard(req.params.id).then(
+      (leaderboard) => {
+        res.send(leaderboard);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  });
+
   // Fetch all games
   app.get('/leaderboard', (req, res) => {
     Leaderboard.fetchLeaderboard().then(
@@ -16,6 +29,18 @@ module.exports = (app) => {
   // Fetch a single players game log
   app.get('/leaderboard/player/:id', (req, res) => {
     Leaderboard.fetchPlayerLeaderboard(req.params.id).then(
+      (leaderboard) => {
+        res.send(leaderboard);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  });
+
+  // Fetch a single players game log
+  app.put('/leaderboard/player/:id', (req, res) => {
+    Leaderboard.updatePlayerLeaderboard(req.params.id).then(
       (leaderboard) => {
         res.send(leaderboard);
       },
