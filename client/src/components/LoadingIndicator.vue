3050fcda22121239f0c7cf3bed75a58edc37bb03
Jay DeLuca <jaydeluca4@gmail.com>
2018-03-29 07:03:48 -0400

    #43 Lay the foundation for the loading animation. We can replace the component with whatever we'd like

---
+++ client/src/components/LoadingIndicator.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="icon" style="width: 100%; text-align: center;" v-if="!dataLoaded">
+    <i class="fas fa-spinner fa-spin"></i>
+   </span>
+</template>
+
+<script>
+  export default {
+    name: 'LoadingIndicator',
+    props: ['dataLoaded']
+  }
+</script>
--- client/src/components/Players.vue
+++ client/src/components/Players.vue
@@ -13,87 +13,99 @@
       </div>
     </section>
 
-    <div v-if="players && players.length > 0">
-      <router-link v-bind:to="{ name: 'NewPlayer' }" class="button is-primary is-fullwidth u-margin--bottom">Add Player</router-link>
-      <div class="columns is-centered">
-        <div class="column is-half">
-          <table class="table is-bordered is-narrow is-hoverable is-fullwidth">
-            <thead>
-            <tr>
-              <th>Name</th>
-              <th>Nickname</th>
-              <th>Chant</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="player in players">
-              <td>{{ player.name }}</td>
-              <td>{{ player.nickname }}</td>
-              <td>{{ player.chant }}</td>
-              <td align="center">
-                <router-link v-bind:to="{ name: 'EditPlayer', params: { id: player._id } }" class="button">Edit</router-link>
-              </td>
-              <td>
-                <a href="#" class="button" @click="deleteAlert(player._id, player.name)">Delete</a>
-              </td>
-            </tr>
-            </tbody>
-          </table>
+    <loading-indicator :data-loaded="dataLoaded"></loading-indicator>
+    <template v-if="dataLoaded">
+      <div v-if="players && players.length > 0">
+        <router-link v-bind:to="{ name: 'NewPlayer' }" class="button is-primary is-fullwidth u-margin--bottom">
+          Add Player
+        </router-link>
+        <div class="columns is-centered">
+          <div class="column is-half">
+            <table class="table is-bordered is-narrow is-hoverable is-fullwidth">
+              <thead>
+              <tr>
+                <th>Name</th>
+                <th>Nickname</th>
+                <th>Chant</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="player in players">
+                <td>{{ player.name }}</td>
+                <td>{{ player.nickname }}</td>
+                <td>{{ player.chant }}</td>
+                <td align="center">
+                  <router-link v-bind:to="{ name: 'EditPlayer', params: { id: player._id } }" class="button">Edit
+                  </router-link>
+                </td>
+                <td>
+                  <a href="#" class="button" @click="deleteAlert(player._id, player.name)">Delete</a>
+                </td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-    <div v-else>
-      <div class="columns is-centered">
-        <div class="column is-4">
-          <div class="card">
-            <div class="card-image">
-              <figure class="image">
-                <img src="../assets/images/nothing2see.gif" alt="Nothing Here">
-              </figure>
-            </div>
-            <div class="card-content">
-              <router-link v-bind:to="{ name: 'NewPlayer' }" class="add_post_link button is-fullwidth is-primary">Add Player</router-link>
+      <div v-else>
+        <div class="columns is-centered">
+          <div class="column is-4">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image">
+                  <img src="../assets/images/nothing2see.gif" alt="Nothing Here">
+                </figure>
+              </div>
+              <div class="card-content">
+                <router-link v-bind:to="{ name: 'NewPlayer' }" class="add_post_link button is-fullwidth is-primary">
+                  Add Player
+                </router-link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
 <script>
   import PlayerService from '@/services/PlayerService'
+
   export default {
     name: 'players',
-    data () {
+    data() {
       return {
         players: [],
         isPaginated: true,
         isPaginationSimple: false,
         defaultSortDirection: 'asc',
         currentPage: 1,
-        perPage: 5
+        perPage: 5,
+        dataLoaded: false
       }
     },
-    mounted () {
+    mounted() {
       this.getPlayers()
     },
     methods: {
       async getPlayers() {
+        this.dataLoaded = false
         const response = await PlayerService.fetchPlayers()
         this.players = response.data.players
+        this.dataLoaded = true
       },
       async deletePlayer(id) {
         await PlayerService.deletePlayer(id)
         this.getPlayers()
-        this.$router.push({ name: 'Players' })
+        this.$router.push({name: 'Players'})
       },
       deleteAlert(id, name) {
         this.$snackbar.open({
           duration: 5000,
-          message: 'Do you want to delete  ' + name + '? ' ,
+          message: 'Do you want to delete  ' + name + '? ',
           type: 'is-danger',
           position: 'is-bottom',
           actionText: 'Yes',
