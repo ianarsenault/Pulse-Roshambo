ec9b3e66b03d5d72584be5ed0e2fa3b8fd94a624
Jay DeLuca <jaydeluca4@gmail.com>
2018-03-31 09:53:52 -0400

    45: updates, cleanups, refactoring landing component

--- server/routes/battle.js
+++ server/routes/battle.js
@@ -1,13 +1,10 @@
+const moment = require("moment")
+
 module.exports = (app) => {
-  // Add new player
   app.post('/battle', (req, res) => {
     res.send(battle(req.body.player1, req.body.player2));
   });
 }
-
-import GameLogsService from '@/services/GameLogsService'
-import LeaderboardService from '@/services/LeaderboardService'
-import moment from 'moment'
 
 function playerChoice(){
   return Math.floor(Math.random() * 3);
@@ -26,7 +23,8 @@ const battle = (player1, player2) => {
 
   let outcome = (3 + p1Throws - p2Throws) % 3;
   let winner = ["Tie!", player1, player2][outcome];
-  let game = {
+
+  return {
     date: moment(),
     playerOne: player1,
     playerTwo: player2,
@@ -34,15 +32,4 @@ const battle = (player1, player2) => {
     throwTwo: choiceName(p2Throws),
     winner: winner
   }
-
-  GameLogsService.addGame(game);
-  LeaderboardService.updateLeaderboards(game);
-
-  return {
-    winner,
-    p1Throws: choiceName(p1Throws),
-    p2Throws: choiceName(p2Throws)
-  }
 }
-
-export default battle;
