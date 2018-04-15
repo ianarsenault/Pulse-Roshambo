58a481a8bc264d56a1e83c0e14bdcb1e937277a8
Jay DeLuca <jaydeluca4@gmail.com>
2018-04-15 15:49:29 -0400

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,12 @@
 
 git pull
 cd client
-npm install
+npm install --production
 npm run build
+mkdir ./dist/static/uploads
 cd ..
 cd server
-npm install
+cp example.env .env
+npm install --production
+forever stopall
+forever start ./bin/www
